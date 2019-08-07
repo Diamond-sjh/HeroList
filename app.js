b35a5de889dd9a23c81a5ae5b5c8039b9d4c0c9a
Diamond-sjh <986694880@qq.com>
2019-08-07 21:17:03 +0800

    完成获取英雄(无分页)和增加英雄

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // 1.1 导入db.js
 const db = require('./db.js');
+// 1.2 添加英雄有文件,所以使用FormData上传,body-parser不能处理FormData的数据类型，所以使用multer模块
+const multer = require('multer');
 
 // 2、---------------创建服务器
 const app = express();
@@ -13,9 +15,37 @@ app.listen(3000, () => console.log('开启服务'));
 // 4、---------------处理请求
 // 4.1 处理静态资源的中间件
 app.use(express.static('manager'));
+app.use('/uploads',express.static(__dirname + '/uploads'));
 
 // 4.2 处理urlencoded 编码格式的请求体
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // 5、--------------------------------------------------各种接口
+// 5.1  获取所有的英雄信息
+app.get('/getHeroes', (req, res) => {
+  let sql = 'select * from heroes order by id desc';
+  db(sql, null, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+})
+
+// 5.2  新增英雄
+const upload = multer({ dest: 'uploads/' });
+app.post('/addHero', upload.single('heroIcon'), function (req, res) {
+  let sql = 'insert into heroes set ?';
+  let values = {
+    name: req.body.heroName,
+    nickname: req.body.heroNickName,
+    skill: req.body.skillName,
+    file: req.file.path
+  }
+  db(sql,values,(err,resule) => {
+    if(err){
+      res.send({code: 201, message: '添加失败'});
+    }else{
+      res.send({code: 200, message: '添加成功'});
+    }
+  })
+})
