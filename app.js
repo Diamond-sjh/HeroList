f5540c8878000b7ca52e2a73e8b7963132b97156
Diamond-sjh <986694880@qq.com>
2019-08-08 17:01:30 +0800

    完成修改英雄信息功能

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.listen(3000, () => console.log('开启服务'));
 // 4、---------------处理请求
 // 4.1 处理静态资源的中间件
 app.use(express.static('manager'));
-app.use('/uploads',express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 // 4.2 处理urlencoded 编码格式的请求体
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,11 +41,56 @@ app.post('/addHero', upload.single('heroIcon'), function (req, res) {
     skill: req.body.skillName,
     file: req.file.path
   }
-  db(sql,values,(err,resule) => {
-    if(err){
-      res.send({code: 201, message: '添加失败'});
-    }else{
-      res.send({code: 200, message: '添加成功'});
+  db(sql, values, (err, resule) => {
+    if (err) {
+      res.send({ code: 201, message: '添加失败' });
+    } else {
+      res.send({ code: 200, message: '添加成功' });
+    }
+  })
+})
+
+// 5.3  根据ID获取英雄信息
+app.get('/getHeroById', (req, res) => {
+  let id = req.query.id;
+  // 如果id为空或者不是数字类型的时候，参数错误，给出提示
+  if (!id || isNaN(id)) {
+    res.send('参数错误');
+    return;
+  }
+  let sql = 'select * from heroes where id=?'
+  db(sql, id, (err, result) => {
+    if (err) throw err;
+    res.send(result[0]);  //  result的值是一个数组
+  })
+})
+
+// 5.4  修改英雄信息
+app.post('/updateHero', upload.single('heroIcon'), function (req, res) {
+  // console.log(req.body);
+  // console.log(req.file);
+  // return;
+  let id = req.body.id;
+  // 如果id为空或者不是数字类型的时候，参数错误，给出提示
+  if (!id || isNaN(id)) {
+    res.send('参数错误');
+    return;
+  }
+  let sql = 'update heroes set ? where id = ?';
+  let values = {
+    name: req.body.heroName,
+    nickname: req.body.heroNickName,
+    skill: req.body.skillName,
+  }
+  // 如果图片没有改动req.file的值为undefined,所以需要单独判断
+  if(req.file){
+    values.file = req.file.path
+  }
+  db(sql, [values, id], (err, result) => {
+    if (err) {
+      res.send({ code: 201, message: '修改失败' });
+    } else {
+      res.send({ code: 200, message: '修改成功' });
     }
   })
 })
